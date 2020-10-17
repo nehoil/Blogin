7eb5fc34e01bfbe85f2e44e89e91211e62f7d383
Nehoray Ilani <neho12@gmail.com>
2020-10-17 20:10:30 +0300

    added modal on read-more click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 function toggleMenu() {
     document.body.classList.toggle('menu-open');
 }
+function toggleReadMore() {
+    document.querySelector('.read-more').classList.toggle('show');
+    document.querySelector('.screen-read-more').classList.toggle('show-screen');
+}
